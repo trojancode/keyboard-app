4cd38e1d05cddd8d9dd3333e3992da1350a4fba6
trojancode <codeboycodes@gmail.com>
2022-07-20 01:17:22 +0530

    removed unwanted code

--- src/handler/storageHandler.ts
+++ src/handler/storageHandler.ts
@@ -1,20 +1,15 @@
 const MIN_TIME = 'minTime'
 
 const saveTimeToLocal = (time: any) => {
-    if (typeof window !== undefined) {
-        window.localStorage.setItem(MIN_TIME, time)
-    }
+    localStorage.setItem(MIN_TIME, time)
 }
 
-const getTimeFromLocal = ():number => {
-    if (typeof window !== undefined) {
-        let mintime=window.localStorage.getItem(MIN_TIME)
-        return mintime?parseInt(mintime):0
-    }
-    return 0;
+const getTimeFromLocal = (): number => {
+    let mintime = localStorage.getItem(MIN_TIME)
+    return mintime ? parseInt(mintime) : 0
 }
 
-export  {
+export {
     saveTimeToLocal,
     getTimeFromLocal
 };
